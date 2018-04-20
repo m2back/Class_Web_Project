a467a3174852cf57cd0d709f44409ea55c1618ce
Mohammad_M2Back <m2back.3d@gmail.com>
2018-04-20 11:06:26 +0400

    Sticky nav bar

--- js/myjquery.js
+++ js/myjquery.js
@@ -18,3 +18,12 @@ $(document).ready(function(){
 
 
 });
+
+
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
